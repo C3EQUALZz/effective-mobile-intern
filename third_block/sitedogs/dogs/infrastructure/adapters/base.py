aa5ae64534b5fdfc09ba32968b8ddee6920d54c3
c3equalz <dan.kovalev2013@gmail.com>
2025-01-24 22:57:39 +0300

    created adapters and commands for future stuff

---
+++ third_block/sitedogs/dogs/infrastructure/adapters/base.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+
+from django.db.models.base import Model
+
+from dogs.domain.entities.base import BaseEntity
+from typing import TypeVar, Generic
+
+ModelType = TypeVar('ModelType', bound=Model)
+EntityType = TypeVar('EntityType', bound=BaseEntity)
+
+
+class BaseAdapter(ABC, Generic[ModelType, EntityType]):
+    @abstractmethod
+    def to_entity(self, model: ModelType) -> EntityType:
+        raise NotImplementedError
+
+    @abstractmethod
+    def to_model(self, entity: EntityType) -> ModelType:
+        raise NotImplementedError
